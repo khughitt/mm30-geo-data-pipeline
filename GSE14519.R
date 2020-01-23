b6edf9b49737ec9ce9a54d6ebcc24d58ea8940bd
Keith Hughitt <keith.hughitt@gmail.com>
2020-01-23 14:45:08 -0500

    Added GSE14519

---
+++ GSE14519.R
@@ -0,0 +1,74 @@
+#!/bin/env/Rscript
+#
+# Expression data from multiple myeloma cells treated with arsenic
+#
+# Matulis et al. (2009)
+#
+library(GEOquery)
+library(tidyverse)
+
+# GEO accession
+accession <- 'GSE14519'
+
+# directory to store raw and processed data
+base_dir <- file.path('/data/human/geo', accession)
+
+raw_data_dir <- file.path(base_dir, 'raw')
+clean_data_dir <- file.path(base_dir, 'processed')
+
+# create output directories if they don't already exist
+for (dir_ in c(raw_data_dir, clean_data_dir)) {
+  if (!dir.exists(dir_)) {
+      dir.create(dir_, recursive = TRUE)
+  }
+}
+
+# download GEO data;
+# result is a list with a single entry containing an ExpressionSet instance
+eset <- getGEO(accession, destdir = raw_data_dir, AnnotGPL = TRUE)[[1]]
+
+# in order to normalize downstream comparisons across datasets, we will
+# aply a size-factor normalization so that the sample sizes all sum to exactly the
+# same amount..
+exprs(eset) <- sweep(exprs(eset), 2, colSums(exprs(eset)), '/') * 1E6
+
+# get relevant sample metadata
+# "treatment_protocol_ch1" is another alias for death;
+# "grow_protocol_ch1" is an alias for relapse;
+sample_metadata <- pData(eset) %>%
+  select(geo_accession, platform_id, title)
+
+# treatment status
+sample_metadata$treatment <- 'Untreated'
+sample_metadata$treatment[grepl('DAR 6', sample_metadata$title)] <- 'DAR (6 hrs)'
+sample_metadata$treatment[grepl('DAR 24', sample_metadata$title)] <- 'DAR (24 hrs)'
+sample_metadata$treatment[grepl('ATO 6', sample_metadata$title)] <- 'ATO (6 hrs)'
+sample_metadata$treatment[grepl('ATO 24', sample_metadata$title)] <- 'ATO (24 hrs)'
+sample_metadata$treatment[grepl('ATO 48', sample_metadata$title)] <- 'ATO (48 hrs)'
+
+# cell line
+sample_metadata$cell_line <- 'U266'
+sample_metadata$cell_line[grepl('MM.1s', sample_metadata$title)] <- 'MM.1s'
+sample_metadata$cell_line[grepl('KMS11', sample_metadata$title)] <- 'KMS11'
+sample_metadata$cell_line[grepl('8226', sample_metadata$title)] <- '8226'
+
+# add cell type and disease (same for all samples)
+sample_metadata$disease <- 'Multiple Myeloma'
+sample_metadata$cell_type <- 'BM-CD138+'
+
+# get expression data and add gene symbol column
+gene_symbols <- gene_symbols <- gsub('///', ' // ', fData(eset)$`Gene symbol`)
+
+expr_dat <- exprs(eset) %>%
+  as.data.frame %>%
+  rownames_to_column('probe_id') %>%
+  add_column(symbol = gene_symbols, .after = 1)
+
+# store cleaned expression data and metadata
+expr_outfile <- file.path(clean_data_dir, sprintf('%s_gene_expr.csv', accession))
+mdat_outfile <- file.path(clean_data_dir, sprintf('%s_sample_metadata.csv', accession))
+
+write_csv(expr_dat, expr_outfile)
+write_csv(sample_metadata, mdat_outfile)
+
+sessionInfo()
