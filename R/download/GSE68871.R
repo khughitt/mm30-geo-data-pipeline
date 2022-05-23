a5cfd50f28f32eba56e70e99fc717555f2905959
Keith Hughitt <keith.hughitt@gmail.com>
2022-05-22 19:34:59 -0700

    ported GSE68871

---
+++ R/download/GSE68871.R
@@ -0,0 +1,12 @@
+#!/bin/env/Rscript
+###############################################################################
+#
+# Download GSE68871
+#
+###############################################################################
+source("R/download/common.R")
+
+acc <- snakemake@wildcards[["acc"]]
+cache_dir <- file.path("/data/raw/geo", acc)
+
+download_geo(acc, cache_dir, snakemake@output[[1]], snakemake@output[[2]], snakemake@output[[3]])
