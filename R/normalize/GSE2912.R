0fb36f831f4d19d33567679ae7cf0c7c11d1ed59
Keith Hughitt <keith.hughitt@gmail.com>
2022-05-25 16:58:22 -0700

    added GSE2912 scripts

---
+++ R/normalize/GSE2912.R
@@ -0,0 +1,48 @@
+#!/bin/env/Rscript
+###############################################################################
+#
+# GSE2912
+#
+###############################################################################
+library(tidyverse)
+
+# load data & metadata
+dat <- read_csv(snakemake@input[[1]], show_col_types = FALSE)
+
+fdata <- read_csv(snakemake@input[[2]], show_col_types = FALSE)
+pdata <- read_csv(snakemake@input[[3]], show_col_types = FALSE)
+
+# size factor normalization (ignore gene symbol column)
+dat[, -1] <- sweep(dat[, -1], 2, colSums(dat[, -1]), '/') * 1E6
+
+# add gene symbol column
+expr_dat <- dat %>%
+  select(-feature) %>%
+  add_column(symbol = fdata$`Gene Symbol`, .before = 1) %>%
+  as_tibble()
+
+mask <- expr_dat$symbol != ""
+expr_dat <- expr_dat[mask, ]
+fdata <- fdata[mask, ]
+
+# split multi-mapped symbols
+expr_dat <- expr_dat %>%
+  separate_rows(symbol, sep = " ?//+ ?")
+
+# columns to include
+sample_metadata <- pdata %>%
+  select(geo_accession, platform_id, title, patient_id,
+         age = `Age (years)`, gender = Sex, mm_stage = Stage)
+
+# add platform & disease stage
+sample_metadata$platform_type <- 'Microarray'
+sample_metadata$disease_stage <- 'MM'
+
+if (!all(colnames(expr_dat)[-1] == sample_metadata$geo_accession)) {
+  stop("Sample ID mismatch!")
+}
+
+# store results
+write_csv(expr_dat, snakemake@output[[1]])
+write_csv(fdata, snakemake@output[[2]])
+write_csv(sample_metadata, snakemake@output[[3]])
