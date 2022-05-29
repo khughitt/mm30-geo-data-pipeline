de235c8e844bea46c232436780cebfb3d3b062cb
Keith Hughitt <keith.hughitt@gmail.com>
2022-05-28 19:21:43 -0700

    add script for package building rule

---
+++ python/build_pkg.py
@@ -0,0 +1,28 @@
+#!/bin/env python
+"""
+Generate datapackages for finalized datasets
+"""
+import frictionless
+import os
+import yaml
+
+# load dataset metadata
+with open(snakemake.input[3]) as fp:
+    mdata = yaml.load(fp, Loader=yaml.FullLoader)
+
+# switch to data dir
+out_dir = os.path.dirname(snakemake.input[0])
+os.chdir(out_dir)
+
+pkg = frictionless.describe("*.csv")
+
+# add dataset metadata
+for key in mdata.keys():
+    # skip "profile" for now (frictionless sets to "datapackage")
+    if key == 'profile':
+        continue
+
+    pkg[key] = mdata[key]
+
+# write datapackage out
+pkg.to_yaml("datapackage.yml")
